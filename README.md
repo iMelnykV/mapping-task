b4cc4f55a8d96586ebaea8c86eab9eb8a622c725
iMelnykV <trxo@mail.ua>
2021-09-20 23:22:12 +0200

    add first 2 lines in readmy and create mapping_task.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mapping-task
+To start this code in some IDE you need to click the button run.
+I spent about 10 minut to create this script. 
---
+++ mapping_task.py
@@ -0,0 +1,43 @@
+import requests
+import schedule
+import datetime
+import time
+
+"""
+Simple data mapping
+
+Your task is to create a simple script that will pull the newest articles
+from the provided API every 5 minutes, then map them into a format defined in
+the models.py file (class Article) and finally print them out.
+We will assess your code performance, readability, reusability and resilience to errors.
+
+The API endpoints you need to use:
+* list of articles: GET https://mapping-test.fra1.digitaloceanspaces.com/data/list.json
+* details of an article: GET https://mapping-test.fra1.digitaloceanspaces.com/data/articles/{article_id}.json
+* media of an article: GET https://mapping-test.fra1.digitaloceanspaces.com/data/media/{article_id}.json
+
+Requirements:
+1. Create a public repository for your code
+2. Create a basic README for your code. In the readme remember to add instructions
+how to start your code and write how long it took to create it
+3. After starting, the script should execute automatically every 5 minutes
+4. The example API has very limited data. Write your solution so that it is optimal
+for processing a few thousand articles per day.
+5. Sections that contain text should be stripped of any html elements, but keeping the content
+"""
+
+def func():
+    response_list = requests.get("https://mapping-test.fra1.digitaloceanspaces.com/data/list.json").json()
+    print(datetime.datetime.now())
+
+    for i in response_list:
+        for x, y in i.items():
+            print("{}: {}".format(x, y))
+    print()
+
+func()
+schedule.every(5).minutes.do(func)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
